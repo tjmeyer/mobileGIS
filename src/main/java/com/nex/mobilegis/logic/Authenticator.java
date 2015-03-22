d4a2829c93ef961fd575c70fb79489e29fd68c9b
Trevor Meyer <trvrmeyer@gmail.com>
2015-03-21 19:12:00 -0600

    finished stabilizing login servlet

---
+++ src/main/java/com/nex/mobilegis/logic/Authenticator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nex.mobilegis.logic;
+
+import com.nex.mobilegis.dataaccess.DBManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author M
+ */
+public class Authenticator {
+    public static Boolean valid;
+    public static int validAccountId;
+    
+    public static Boolean Authenticate(String username, String password) throws SQLException, ClassNotFoundException{
+        DBManager db = DBManager.getInstance();
+        String query = "SELECT account_id, password FROM user WHERE username = \"" + username + "\"";
+        ResultSet rs = db.execute(query);
+        if(rs.next())
+        {
+            String validPassword = rs.getString("password");
+            valid = validPassword.equals(password);
+        }
+        else
+        {
+            valid = false;
+        }
+        
+        if(valid)
+        {
+            validAccountId = rs.getInt("account_id");
+        }
+        
+        return valid;
+    }
+}
