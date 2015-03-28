c5c99224f6962b6dd4398361b1a1a9fb59d90fa2
Trevor Meyer <trvrmeyer@gmail.com>
2015-03-27 23:54:39 -0600

    more styling!

--- src/main/java/com/nex/mobilegis/dataaccess/DBManager.java
+++ src/main/java/com/nex/mobilegis/dataaccess/DBManager.java
@@ -14,21 +14,21 @@ import java.util.logging.Logger;
  * @author M
  */
 public class DBManager {
-    // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-    static final String DB_URL = "jdbc:mysql://localhost/nex";
-    
-    //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "kumite2";
-    
 //    // JDBC driver name and database URL
 //    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-//    static final String DB_URL = "jdbc:mysql://127.7.255.2:3306/java";
+//    static final String DB_URL = "jdbc:mysql://localhost/nex";
 //    
 //    //  Database credentials
-//    static final String USER = "adminzXPkWVV";
-//    static final String PASS = "CnPzN5ksVQAE";
+//    static final String USER = "root";
+//    static final String PASS = "kumite2";
+    
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL = "jdbc:mysql://127.7.255.2:3306/java";
+    
+    //  Database credentials
+    static final String USER = "adminzXPkWVV";
+    static final String PASS = "CnPzN5ksVQAE";
     
     // This is a singleton class
     static DBManager singletonInstance = null;
